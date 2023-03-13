84951cccc41eed2d69507bcaf6918723f2b0905a
Nikolay S <niko.sych@yandex.ru>
2023-03-13 11:27:23 +0300

    Выполнена Задача 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
+  const checkArr1 = arr1.every((element, index) => element === arr2[index]);
+  const checkArr2 = arr2.every((element, index) => element === arr1[index]);
+  return checkArr1 && checkArr2;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  return users
+    .filter((element) => element.gender === gender)
+    .reduce((acc, element, index, arr) => {
+      acc += element.age;
+      if (index >= arr.length - 1) {
+        return acc / arr.length;
+      } else {
+        return acc;
+      }
+    }, 0);
 }
